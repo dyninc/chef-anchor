28872b1aaf37362b7aabc308de2cee05cbec0726
Paul Thomas <pthomas@dyn.com>
2016-05-03 16:45:53 +0100

    Add anchor_certificate resource

---
+++ libraries/anchor_certificate.rb
@@ -0,0 +1,131 @@
+module AnchorCookbook
+  class AnchorCertificate < ChefCompat::Resource
+    require 'openssl'
+    require 'uri'
+    require 'net/http'
+    require_relative 'certificate_helpers'
+
+    include AnchorCookbook::AnchorHelpers::Certificate
+
+    use_automatic_resource_name
+
+    provides :anchor_certificate
+
+    ################
+    ## Properties ##
+    ################
+    property :cn, String, name_property: true
+    property :country, String, required: true
+    property :state, String, required: true
+    property :city, String, default: ""
+    property :organisation, String, default: ""
+    property :department, String, default: ""
+    property :email, String, default: ""
+
+    # For my american colleagues
+    alias :organization :organisation
+
+    # These map the friendly names above to the OpenSSL field names
+    alias :C :country
+    alias :ST :state
+    alias :L :city
+    alias :O :organisation
+    alias :OU :department
+    alias :CN :cn
+    alias :emailAddress :email
+
+    # Size for the private key
+    property :bits, Integer, default: 2048
+
+    # URI & Credentials for talking to anchor
+    property :anchorurl, String, required: true, desired_state: false
+    property :anchoruser, String, required: true, desired_state: false
+    property :anchorsecret, String, required: true, desired_state: false
+
+    # Where to place the certificate and key, owner, permissions etc.
+    property :path, String, required: true, desired_state: false
+    property :keyfile, String, default: 'key.pem'
+    property :certfile, String, default: 'certificate.pem'
+    property :owner, [String, Integer], default: 'root'
+    property :group, [String, Integer], default: 'root'
+    property :mode, [String, Integer], default: 0600
+
+    #########################
+    ## Load current value  ##
+    #########################
+
+    load_current_value do |new_resource|
+      # Get paths
+      keypath = ::File.join(new_resource.path, new_resource.keyfile)
+      certpath = ::File.join(new_resource.path, new_resource.certfile)
+
+      # Only if the certificate and key exist attempt to populate
+      # the current_resource with the values
+      if ::File.exist?(keypath) && ::File.exist?(certpath)
+
+        certificate = OpenSSL::X509::Certificate.new IO.read(certpath)
+        key = OpenSSL::PKey::RSA.new IO.read(keypath)
+
+        # Check the key matches the certificate
+        current_value_does_not_exist! unless certificate.check_private_key(key)
+
+        # Load each field in the SSL certificate into the current_resource
+        # for comparison
+        certificate.subject.to_a.each { |field| send(field[0], field[1]) }
+      else
+        Chef::Log.debug "Certificate or Key file did not exist"
+        current_value_does_not_exist!
+      end
+    end
+
+    #############
+    ## Actions ##
+    #############
+
+    action :create do
+      # Initialise these here
+      # Then try to load them in the converge_if_changed block
+      key = nil
+      certificate = nil
+
+      keyfile = ::File.join(new_resource.path, new_resource.keyfile)
+      certfile = ::File.join(new_resource.path, new_resource.certfile)
+
+      converge_if_changed do
+        # Key for the new certificate signing request
+        key = OpenSSL::PKey::RSA.new new_resource.bits
+
+        # Generate a new signing request
+        request = generate_csr(new_resource, key)
+
+        # Submit the CSR to anchor
+        certificate = submit_csr(
+          request,
+          {
+            url: new_resource.anchorurl,
+            user: new_resource.anchoruser,
+            secret: new_resource.anchorsecret
+          }
+        )
+      end
+
+      file keyfile do
+        owner new_resource.owner
+        group new_resource.group
+        mode new_resource.mode
+        content key.to_pem unless key.nil?
+        sensitive true
+        action :create
+      end
+
+      file certfile do
+        owner new_resource.owner
+        group new_resource.group
+        mode new_resource.mode
+        content certificate unless certificate.nil?
+        sensitive true
+        action :create
+      end
+    end
+  end
+end
---
+++ libraries/certificate_helpers.rb
@@ -0,0 +1,41 @@
+module AnchorCookbook
+  module AnchorHelpers
+    module Certificate
+      def generate_subject(new_resource)
+        OpenSSL::X509::Name.new([
+          ['C', new_resource.country, OpenSSL::ASN1::PRINTABLESTRING],
+          ['ST', new_resource.state, OpenSSL::ASN1::PRINTABLESTRING],
+          ['L', new_resource.city, OpenSSL::ASN1::PRINTABLESTRING],
+          ['O', new_resource.organisation, OpenSSL::ASN1::UTF8STRING],
+          ['OU', new_resource.department, OpenSSL::ASN1::UTF8STRING],
+          ['CN', new_resource.cn, OpenSSL::ASN1::UTF8STRING],
+          ['emailAddress', new_resource.email, OpenSSL::ASN1::UTF8STRING]
+        ])
+      end
+
+      def generate_csr(new_resource, key)
+        # Create a new CSR
+        request = OpenSSL::X509::Request.new
+        request.version = 0
+        request.subject = generate_subject(new_resource)
+        request.public_key = key.public_key
+        request.sign(key, OpenSSL::Digest::SHA256.new)
+
+        request
+      end
+
+      def submit_csr (request, anchor={})
+        # Send the CSR to Anchor
+        Net::HTTP.post_form(
+          URI.parse(anchor[:url]),
+          {
+            user: anchor[:user],
+            secret: anchor[:secret],
+            encoding: 'PEM',
+            csr: request.to_pem
+          }
+        ).body
+      end
+    end
+  end
+end
